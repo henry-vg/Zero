ab42a03af23122f14885cac5a830522e2541030a
henry-vg <hvg.henry@gmail.com>
2026-09-05 02:05:04 -0300

    chore: minor adjustments in main execution

--- src/host/main.py
+++ src/host/main.py
@@ -2,16 +2,23 @@ from src.host.kernel.bit import Bit
 from src.host.kernel.nand import nand
 
 
-def main() -> None:
+def main() -> str:
     a = Bit(1)
     b = Bit(0)
 
     result = nand(a, b)
 
-    print(f"a    = {a}")
-    print(f"b    = {b}")
-    print(f"NAND = {result}")
+    return "\n".join(
+        [
+            f"a    = {a}",
+            f"b    = {b}",
+            f"NAND = {result}",
+        ]
+    )
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        print(main())
+    except Exception as e:
+        print(f"[FATAL] {e}")
